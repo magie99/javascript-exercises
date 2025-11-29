a8221a9e974862ecbc7df05cc00dbf6be2639f1f
magie99 <119002695+magie99@users.noreply.github.com>
2025-11-29 12:34:31 +0100

    Solve 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let newString = "";
+    if (num < 0) return "ERROR";
+    while (num > 0){
+        newString+= string;
+        num--;
+    }
+    return newString;
+    
 };
 
 // Do not edit below this line
