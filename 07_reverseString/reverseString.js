a5a94f8ea775cfcb0c739249a9c291b9fa6711e5
magie99 <github_46432@maxi-login.de>
2025-12-02 17:11:31 +0100

    Solve 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let reverse = "";
+    let i = word.length;
 
+    while (i >= 0){
+        reverse += word.charAt(i);
+        i--;
+    }
+    return reverse;
 };
 
 // Do not edit below this line
