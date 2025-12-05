71d0b619f59c26dfd9514d2a55cedfadf2e1b5c3
magie99 <github_46432@maxi-login.de>
2025-12-05 15:49:03 +0100

    Solve 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove){ 
+    let newArray = [];
+    array.forEach(element => {
+        if (!remove.includes(element)){
+            newArray.push(element)
+        }
+    });
+    console.log(newArray)
+    return newArray;
 };
 
 // Do not edit below this line
