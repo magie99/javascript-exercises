3600aac7115407c8e329a4c4fafa86170cdc748e
magie99 <119002695+magie99@users.noreply.github.com>
2025-12-06 17:23:01 +0100

    Solve 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(x, y) {
+	z = x + y;
+  return z;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	z = x - y;
+  return z;
 };
 
-const sum = function() {
-	
+const sum = function(x) {
+  let sum = 0;
+	for (let i = 0; i < x.length; i++){
+    sum += x[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(x) {
+  let sum = 1;
+	for (let i = 0; i < x.length; i++){
+    sum *= x[i];
+  }
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	z = Math.pow(x, y);
+  return z;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let fac = 1;
+	for ( x; x > 0; x--){
+    fac *= x;
+  }
+  return fac;
 };
 
 // Do not edit below this line
