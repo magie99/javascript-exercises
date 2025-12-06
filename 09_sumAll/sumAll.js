c4ed4f52c4dbe6b823e535e8a2c2b36acf9d5b72
magie99 <github_46432@maxi-login.de>
2025-12-06 10:36:54 +0100

    Solve 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,32 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    if (!(Number.isInteger(x) && Math.sign(x) == 1) || !(Number.isInteger(y) && Math.sign(y) == 1)){
+        return "ERROR";
+    }
+    let start;
+    let end;
+    let diff;
+    let sum;
+    let step;
+
+    if (x < y){
+        start = x;
+        end = y;
+    }
+    else if (x > y){
+        start = y;
+        end = x;
+    }
+
+    diff = end -start;
+    sum = start;
+    step = start + 1;
+
+    for (let i = diff; i > 0; i--){
+        sum += step;
+        step ++;
+    }
+
+    return sum;
 
 };
 
