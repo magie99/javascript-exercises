9f89f1cbc136b8ab97a8f851eaabd17d26b35f52
magie99 <119002695+magie99@users.noreply.github.com>
2025-12-06 19:23:00 +0100

    solve 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    let array = [...input];
+    array = array.filter(char => {
+        return /[a-zA-Z0-9]/.test(char);
+    });
+    console.log(array);
+    let n = array.length - 1;
+    for (let i = 0; i < n;){
+        if (array[i].toLowerCase() != array[n].toLowerCase()){
+            return false;
+        }
+        i++;
+        n--;
+    }
+    return true;
 };
 
 // Do not edit below this line
