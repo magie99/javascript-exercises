7cc009f760d42dee64edcbd0cc075a1b11bea222
magie99 <119002695+magie99@users.noreply.github.com>
2026-01-10 16:46:24 +0100

    Solved 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
-
+function fibonacci(x){
+    Number(x)
+    let previous = 1;
+    let result = 1;
+    let temp;
+    if(x === NaN || x < 0){
+        return "OOPS"
+    }
+    else if(x == 0){
+        return 0;
+    }
+    else{
+        for(i = 2; i < x; i++){
+            temp = result;
+            result += previous;
+            previous = temp;
+        }
+        console.log(result);
+        return result;
+        }
 };
 
 // Do not edit below this line
