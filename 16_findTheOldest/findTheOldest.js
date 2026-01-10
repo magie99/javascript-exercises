6c4a11dbad37fb2c617d819fbf97d4ccfddcbf4f
magie99 <119002695+magie99@users.noreply.github.com>
2026-01-10 17:17:20 +0100

    Solve 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    for (let person of people){
+        let age;
+        if(person.yearOfDeath){
+            age = person.yearOfDeath - person.yearOfBirth
+        }
+        else{
+            age = new Date().getFullYear() - person.yearOfBirth
+        }
+        person.age = age;
+    }
+    people.sort((a,b) => b.age - a.age);
+    console.table(people);
+    return people[0];
 };
 
 // Do not edit below this line
